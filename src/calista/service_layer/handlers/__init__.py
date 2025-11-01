6cc311e484e53c6e7b9452327a5f35274c7a56d5
mhallum <mkhallum92@gmail.com>
2025-11-01 10:56:59 -0700

    refactor(handlers): simplify COMMAND_HANDLERS initialization using unpacking

--- src/calista/service_layer/handlers/__init__.py
+++ src/calista/service_layer/handlers/__init__.py
@@ -4,5 +4,4 @@ from collections.abc import Callable
 
 from .catalog_handlers import COMMAND_HANDLERS as CATALOG_COMMAND_HANDLERS
 
-COMMAND_HANDLERS: dict[type, Callable[..., None]] = {}
-COMMAND_HANDLERS.update(CATALOG_COMMAND_HANDLERS)
+COMMAND_HANDLERS: dict[type, Callable[..., None]] = {**CATALOG_COMMAND_HANDLERS}
