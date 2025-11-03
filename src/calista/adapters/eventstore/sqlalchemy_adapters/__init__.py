79f99a0622a256beacdbb2e77b17e498b8a412fb
mhallum <mkhallum92@gmail.com>
2025-11-03 12:13:11 -0800

    feat(adapters): introduce in-memory and SQLAlchemy adapter packages with initial documentation

---
+++ src/calista/adapters/eventstore/sqlalchemy_adapters/__init__.py
@@ -0,0 +1,6 @@
+"""Defines the SQLAlchemy EventStore adapter package.
+
+This package contains an SQLAlchemy-based implementation of an event store and related components.
+The SQLAlchemy event store provides durable storage of events using a relational database,
+enabling reliable event sourcing and persistence.
+"""
